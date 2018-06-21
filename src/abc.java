cf17edf6d2488c928800870051232b4069462279
Chen <Chen591286869@163.com>
2018-06-21 17:19:29 +0800

    cdj3

--- src/abc.java
+++ src/abc.java
@@ -3,4 +3,11 @@ public class abc {
     public void Chen(){
         System.out.println("6666");
     }
+    public void Chen3(){
+        System.out.println("4444");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("9999");
+    }
 }
